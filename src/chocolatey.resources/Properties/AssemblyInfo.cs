067dbaa7415816485484fbfb93e59553e2db5979
Victor Milovanov <lostfreeman@gmail.com>
2020-02-29 14:11:10 -0800

    resources target .NET Standard 1.0

--- src/chocolatey.resources/Properties/AssemblyInfo.cs
+++ src/chocolatey.resources/Properties/AssemblyInfo.cs
@@ -21,5 +21,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-
+#if !NETSTANDARD1_0
 [assembly: Guid("50541cb5-0537-464a-9cf2-3e93dae63703")]
+#endif
