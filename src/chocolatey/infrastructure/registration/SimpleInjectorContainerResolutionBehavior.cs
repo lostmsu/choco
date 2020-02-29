dae9cebb6f306f899d8bcc334ab77e9480b04b5e
Victor Milovanov <lostfreeman@gmail.com>
2020-02-29 15:04:13 -0800

    updated code for newer SimpleInjector (untested)

--- src/chocolatey/infrastructure/registration/SimpleInjectorContainerResolutionBehavior.cs
+++ src/chocolatey/infrastructure/registration/SimpleInjectorContainerResolutionBehavior.cs
@@ -43,28 +43,24 @@ namespace chocolatey.infrastructure.registration
         ///   Gets the given <paramref name="implementationType" />'s constructor that can be used by the
         ///   container to create that instance.
         /// </summary>
-        /// <param name="serviceType">Type of the abstraction that is requested.</param>
         /// <param name="implementationType">Type of the implementation to find a suitable constructor for.</param>
         /// <returns>
         ///   The <see cref="T:System.Reflection.ConstructorInfo" />.
         /// </returns>
         /// <exception cref="T:SimpleInjector.ActivationException">Thrown when no suitable constructor could be found.</exception>
-        public ConstructorInfo GetConstructor(Type serviceType, Type implementationType)
+        public ConstructorInfo GetConstructor(Type implementationType)
         {
-            if (serviceType.IsAssignableFrom(implementationType))
-            {
-                var longestConstructor = (from constructor in implementationType.GetConstructors()
-                                          orderby constructor.GetParameters().Count() descending
-                                          select constructor).Take(1);
+            var longestConstructor = (from constructor in implementationType.GetConstructors()
+                                      orderby constructor.GetParameters().Count() descending
+                                      select constructor).Take(1);
 
-                if (longestConstructor.Any())
-                {
-                    return longestConstructor.First();
-                }
+            if (longestConstructor.Any())
+            {
+                return longestConstructor.First();
             }
 
             // fall back to the container's default behavior.
-            return _originalBehavior.GetConstructor(serviceType, implementationType);
+            return _originalBehavior.GetConstructor(implementationType);
         }
     }
 }
